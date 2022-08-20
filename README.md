e76c0615444ae194c13e5c6887e5ae7681bca6eb
danielbatres <danielbatresuser@gmail.com>
2022-08-20 14:16:33 -0600

    How to create API with NextJS

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nextjs-avocado
+# nextjs
---
+++ pages/api/avo/index.ts
@@ -0,0 +1,14 @@
+import { IncomingMessage, ServerResponse } from "http";
+import Database from "@database";
+
+const allAvos = async (req: IncomingMessage, res: ServerResponse) => {
+  const db = new Database();
+  const allEntries = await db.getAll();
+  const length = allEntries.length;
+
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'application/json');
+  res.end(JSON.stringify({data: allEntries, length}));
+}
+
+export default allAvos;
